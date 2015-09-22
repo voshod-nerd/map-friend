8a7c4e58ab79210758f26e29653e1713e4497adb
klov <klovkrol@gmail.com>
2015-09-22 22:35:09 +0600

    рефактор

--- html (copy)/src/search_by_label.js
+++
@@ -1,8 +0,0 @@
-function get_label_for_map(){
-$.ajax({
-  url: "http://10.211.55.10/basic/web/index.php?r=site/label",
-  success: function(msg){
-    alert( "Прибыли данные: " + msg );
-  }
-});
-}
